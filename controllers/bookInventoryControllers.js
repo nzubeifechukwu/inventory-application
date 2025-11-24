654c79810c89b9675035ebf0cee838737f9e1f7f
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2025-11-24 05:20:13 +0100

    Write navbar partial

--- controllers/bookInventoryControllers.js
+++ controllers/bookInventoryControllers.js
@@ -1,8 +1,9 @@
 const db = require("../db/queries");
+const { title, links } = require("../utils/utils");
 
 async function getAllStock(req, res) {
   const books = await db.getAllStock();
-  res.render("index", { books: books, title: "Book Inventory" });
+  res.render("index", { books: books, title: title, links: links });
 }
 
 module.exports = { getAllStock };
---
+++ utils/utils.js
@@ -0,0 +1,7 @@
+const title = "Book Inventory";
+const links = [
+  { href: "/", text: "Home" },
+  { href: "new", text: "Add Book" },
+];
+
+module.exports = { links, title };
